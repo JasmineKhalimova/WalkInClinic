1def5b05544fadc3c358174c6ea6e056f68bbd2a
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2018-09-04 18:27:54 -0400

    walk in clinic Cork ireland

---
+++ index.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Wilton Walk In Clinic</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,800,700,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=BenchNine:300,400,700' rel='stylesheet' type='text/css'>
+	<script src="js/modernizr.js"></script>
+</head>
+<body>
+	<header class="top-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-5 header-logo">
+					<br>
+					<a href="#home"><img src="img/logo.png" alt="" class="img-responsive logo"></a>
+				</div>
+
+				<div class="col-md-7">
+					<nav class="navbar navbar-default">
+					  <div class="container-fluid nav-bar">
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					    </div>
+					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					      
+					      <ul class="nav navbar-nav navbar-right">
+					        <li><a class="menu active" href="#home" >Home</a></li>
+					        <li><a class="menu" href="#about">about us</a></li>
+					        <li><a class="menu" href="#service">our services </a></li>
+					        <li><a class="menu" href="#contact"> contact us</a></li>
+					      </ul>
+					    </div>
+					  </div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<section class="slider" id="home">
+		<div class="container-fluid">
+			<div class="row">
+			    <div id="carouselmain" class="carousel slide carousel-fade" data-ride="carousel">
+					<div class="header-backup"></div>
+			        <!-- Wrapper for slides -->
+			        <div class="carousel-inner" role="listbox">
+			            <div class="item active">
+			            	<img src="img/slide-one.jpg" alt="">
+			                <div class="carousel-caption">
+		               			<h1>Walk-in</h1>
+		               			<p>No Need for Appointments to see a doctor</p>
+		               			<button><a href="#about">Learn More</a></button>
+			                </div>
+			            </div>
+			            <div class="item">
+			            	<img src="img/slide-two.jpg" alt="">
+			                <div class="carousel-caption">
+		               			<h1>Providing</h1>
+		               			<p>High-quality service for the whole family</p>
+		               			<button><a href="#service">Learn More</a></button>
+			                </div>
+			            </div>
+			            <div class="item">
+			            	<img src="img/slide-three.jpg" alt="">
+			                <div class="carousel-caption">
+		               			<h1>Travel Vaccine</h1>
+		               			<p>We offer convenient and affordable travel vaccinations</p>
+		               			<button><a href="#service">Learn More</a></button>
+			                </div>
+			            </div>
+			            <div class="item">
+			            	<img src="img/slide-four.jpg" alt="">
+			                <div class="carousel-caption">
+		               			<h1>Full Screening</h1>
+		               			<p>Because prevention is always better than a cure</p>
+		               			<button><a href="#service">Learn More</a></button>
+			                </div>
+			            </div>
+			        </div>
+			        <!-- Controls -->
+			        <a class="left carousel-control" href="#carouselmain" role="button" data-slide="prev">
+			            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			            <span class="sr-only">Previous</span>
+			        </a>
+			        <a class="right carousel-control" href="#carouselmain" role="button" data-slide="next">
+			            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			            <span class="sr-only">Next</span>
+			        </a>
+			    </div>
+			</div>
+		</div>
+	</section><!-- end of slider section -->
+
+	<!-- about section -->
+	<section class="about text-center" id="about">
+		<div class="container">
+			<div class="row">
+				<h2>about us</h2>
+				<h4>Wilton walk-in clinic is a newly established clinic to offer friendly, convenient and affordable services provided by experienced and skilled healthcare professionals. We offer not only a Walk-In service but are now also offering a number of other clinics alongside this.</h4>
+				<div class="col-md-4 col-sm-6">
+					<div class="single-about-detail clearfix">
+						<div class="about-img">
+							<img class="img-responsive" src="img/item1.jpg" alt="">
+						</div>
+						<div class="about-details">
+							<div class="pentagon-text">
+								<h1>S</h1>
+							</div>
+							<h3>Full Medical Screening</h3>
+							<p>Prevention is better than a cure and so here at the walk-in clinic, we are offering the following services to help you take control of your health. We offer appointments targeting men’s health and women’s health as well as diabetes and blood pressure checks. Our doctors are experienced and helpful in the area of prevention and want to help you stay well long into the future.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6">
+					<div class="single-about-detail">
+						<div class="about-img">
+							<img class="img-responsive" src="img/item2.jpg" alt="">
+						</div>
+						<div class="about-details">
+							<div class="pentagon-text">
+								<h1>V</h1>
+							</div>
+
+							<h3>Travel Vaccination</h3>
+							<p>We offer convenient and affordable travel vaccinations in Cork and medications for when you are planning that big trip. You must book in advance for travel vaccines, please book an appointment. It is important to remember that some vaccinations require a ‘schedule’ which means that you will need more than one vaccination over a period of time before you go away.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-6">
+					<div class="single-about-detail">
+						<div class="about-img">
+							<img class="img-responsive" src="img/item3.jpg" alt="">
+						</div>
+						<div class="about-details">
+							<div class="pentagon-text">
+								<h1>B</h1>
+							</div>
+							<h3>Blood Pressure Checks</h3>
+							<p>Hypertension (high blood pressure) is a serious risk factor for stroke and heart disease. It is important to ensure that your blood pressure is well controlled. We offer blood pressure checks as well as blood pressure clinics. Wilton walk-in clinic also offers convenient 24 hours blood pressure monitor. No need for appointments to see a doctor to get your blood pressure.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- end of about section -->
+
+
+	<!-- service section starts here -->
+	<section class="team" id="service">
+		<div class="container">
+			<div class="row">
+				<div class="team-heading text-center">
+					<h2>Our Services</h2>
+					<h4>Whilst Wilton clinic started as a walk-in medical clinic we have responded to our patient's requests by continually updating our service. We now offer many other services to complement our walk-in clinic. This includes our Travel Vaccine Clinic, Cosmetic Clinic and many other services as you can see below.</h4>
+				</div>
+				<div class="col-md-2 single-member col-sm-4">
+					<div class="person">
+						<img class="img-responsive" src="img/member1.jpg" alt="member-1">
+					</div>
+					<div class="person-detail">
+						<div class="arrow-bottom"></div>
+						<h3>PRESCRIPTIONS</h3>
+						<p>You can come to our walk-in clinic at any time for your pill refill. However, if you want to minimize your waiting time you can book with our nurse for this service also.</p>
+					</div>
+				</div>
+				<div class="col-md-2 single-member col-sm-4">
+					<div class="person-detail">
+						<div class="arrow-top"></div>
+						<h3>EAR SYRINGING</h3>
+						<p>Earwax buildup can cause damage to these structures over time. This can affect your hearing. We can remove excess earwax with ear irrigation.</p>
+					</div>
+					<div class="person">
+						<img class="img-responsive" src="img/member2.jpg" alt="member-2">
+					</div>
+				</div>
+				<div class="col-md-2 single-member col-sm-4">
+					<div class="person">
+						<img class="img-responsive" src="img/member3.jpg" alt="member-3">
+					</div>
+					<div class="person-detail">
+						<div class="arrow-bottom"></div>
+						<h3>HYPERTENSION</h3>
+						<p>Hypertension is a serious risk factor for stroke and heart disease. It is important to ensure that your blood pressure is well controlled. Keep blood pressure checks.</p>
+					</div>
+				</div>
+				<div class="col-md-2 single-member col-sm-4">
+					<div class="person-detail">
+						<div class="arrow-top"></div>
+						<h3>FLU JABS</h3>
+						<p>Flu Jabs are particularly important for those with chronic health conditions such as diabetes and asthma. They also help prevent the spread of flu.</p>
+					</div>
+					<div class="person">
+						<img class="img-responsive" src="img/member4.jpg" alt="member-4">
+					</div>
+				</div>
+				<div class="col-md-2 single-member col-sm-4">
+					<div class="person">
+						<img class="img-responsive" src="img/member5.jpg" alt="member-5">
+					</div>
+					<div class="person-detail">
+						<div class="arrow-bottom"></div>
+						<h3>SMEAR CLINICS </h3>
+						<p>It’s very important that you keep up with your Cervical Screening schedule. In order to make this as easy as possible, we offer evening appointments as well.</p>
+					</div>
+				</div>
+				<div class="col-md-2 single-member col-sm-4">
+					<div class="person-detail">
+						<div class="arrow-top"></div>
+						<h3>BLOOD TESTS</h3>
+						<p>We offer phlebotomy services at our clinic. We primarily take blood for our own doctors but also offer a service where we only take the blood.</p>
+					</div>
+					<div class="person">
+						<img class="img-responsive" src="img/member6.jpg" alt="member-5">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- end of services section -->
+
+	<!-- map section -->
+	<div class="api-map" id="contact">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12 map" id="map"></div>
+			</div>
+		</div>
+	</div><!-- end of map section -->
+
+	<!-- contact section starts here -->
+	<section class="contact">
+		<div class="container">
+			<div class="row">
+				<div class="contact-caption clearfix">
+					<div class="contact-heading text-center">
+						<h2>contact us</h2>
+					</div>
+					<div class="col-md-5 contact-info text-left">
+						<h3>contact information</h3>
+						<div class="info-detail">
+							<ul><li><i class="fa fa-calendar"></i><span>Monday - Friday:</span> 9:30 AM to 7:30 PM</li></ul>
+							<ul><li><i class="fa fa-map-marker"></i><span>Address:</span>Wilton Court, Cork, ireland.</li></ul>
+							<ul><li><i class="fa fa-phone"></i><span>Phone:</span>+353 21 422 7200</li></ul>
+							<ul><li><i class="fa fa-envelope"></i><span>Email:</span> wiltonwalkinclinic@gmail.com</li></ul>
+						</div>
+					</div>
+					<div class="col-md-6 col-md-offset-1 contact-form" id="form">
+						<h3>Get in touch.</h3>
+
+						<?php if(isset($_GET["responseMessage"])): ?>
+                    		<div class="alert-success"><?php echo $_GET["responseMessage"]; ?></div>
+                		<?php endif; ?>
+					<form action="php/sendmail.php" method="post">
+						<div class="row form-group">
+							<div class="col-md-6">
+								<label for="name">* Name</label>
+								<input type="text" name="name" id="name" class="form-control" placeholder="Name" required="required">
+							</div>
+							<div class="col-md-6">
+								<label for="lname">* Phone number</label>
+								<input type="text" name="phone" id="lname" class="form-control" placeholder="Phone number" required="required">
+							</div>
+						</div>
+
+						<div class="row form-group">
+							<div class="col-md-12">
+								<label for="email">* Email</label>
+								<input type="email" name="email" id="email" class="form-control" placeholder="Your email address" required="required">
+							</div>
+						</div>
+
+						<div class="row form-group">
+							<div class="col-md-12">
+								<label for="message">* Message</label>
+								<textarea name="message" name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Message" required="required"></textarea>
+							</div>
+						</div>
+						<div class="form-group">
+							<input type="submit" value="Send Message" class="btn btn-primary">
+						</div>
+
+					</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- end of contact section -->
+
+	<!-- footer starts here -->
+	<footer class="footer clearfix">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6 footer-para">
+					<p>&copy; 2018. Wilton Walk In Clinc. All right reserved <br> Developed by:<a href="https://web-brickdesign.com" target="_blank"> Web-Brick Design</a></p>
+				</div>
+				<div class="col-xs-6 text-right">
+					<a href="https://www.facebook.com/wiltonwalkin/" target="_blank"><i class="fa fa-facebook"></i></a>
+					<a href="https://www.instagram.com/wiltonwalkinclinic/" target="_blank"><i class="fa fa-instagram"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- script tags
+	============================================================= -->
+	<script src="js/jquery-2.1.1.js"></script>
+	<script src="js/gmaps.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+</html>
